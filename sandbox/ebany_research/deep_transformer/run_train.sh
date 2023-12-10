ed96e6b18b657cf9c4606dabf285cce005056a4b
dmitrymailk <dmitrymail_k@mail.ru>
2023-12-10 22:16:02 +0000

    delete useless

--- sandbox/ebany_research/deep_transformer/run_train.sh
+++ sandbox/ebany_research/deep_transformer/run_train.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=1
 
 # --model_name_or_path EleutherAI/pythia-70m \
-python -m copy_weights.run_clm_no_trainer \
+python -m deep_transformer.run_clm_no_trainer \
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
 	--config_name EleutherAI/pythia-70m \
@@ -9,6 +9,6 @@ python -m copy_weights.run_clm_no_trainer \
     --output_dir ./models/ \
 	--num_train_epochs 4 \
 	--gradient_accumulation_steps 4 \
-	--per_device_train_batch_size 16 \
+	--per_device_train_batch_size 2 \
 	--report_to wandb \
 	--with_tracking
