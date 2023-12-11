53091b3c13dc962a9ec9c45d7de0e404595ca365
dmitrymailk <dmitrymail_k@mail.ru>
2023-12-11 07:18:57 +0000

    update permissions

--- 1_deepspeed_full_finetune/1_1_vanilla_deepspeed/run_ds.sh
+++ 1_deepspeed_full_finetune/1_1_vanilla_deepspeed/run_ds.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=2
-deepspeed --bind_cores_to_rank 1_1_vanilla_deepspeed.py --checkpoint_dir experiment_deepspeed $@
+deepspeed --bind_cores_to_rank --include localhost:0 1_1_vanilla_deepspeed.py --checkpoint_dir experiment_deepspeed $@
